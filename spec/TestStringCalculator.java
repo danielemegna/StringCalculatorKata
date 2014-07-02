d58c3a2abd4959f142dd0dda937ed612d7629505
Daniele Megna <daniele.megna@nyxsolutions.it>
2014-07-02 11:41:56 +0200

    Created Test class and first class of string calculator

---
+++ spec/TestStringCalculator.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class TestStringCalculator
+{
+
+	@Test
+	public void assertingThatTrueIsTrue()
+	{
+		org.junit.Assert.assertTrue(true);
+	}
+
+	@Test
+	public void stringCalculatorReturnMeTrueShouldReturnTrue()
+	{
+		StringCalculator sc = new StringCalculator();
+		//Assert.assertTrue(sc.returnMeTrue());
+	}
+}
---
+++ spec/runtests.sh
@@ -0,0 +1,1 @@
+javac -cp '.:/usr/share/java/junit4-4.11.jar' ../StringCalculator.java TestStringCalculator.java && java -cp '.:../:/usr/share/java/junit4-4.11.jar' org.junit.runner.JUnitCore TestStringCalculator
